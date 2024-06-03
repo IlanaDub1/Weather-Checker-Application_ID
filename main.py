a5752351a72fd1f5e3f5a9ad0de9650ddd689862
IlanaDub1 <61084401+IlanaDub1@users.noreply.github.com>
2024-06-03 11:09:22 +0300

    new2

--- main.py
+++ main.py
@@ -13,6 +13,9 @@ def get_weather(city):
 
 # Streamlit UI
 st.title("Weather App")
+name = st.text_input('Enter your name', '')
+if name:
+    st.write(f'Hello {name}, welcome to the weather app!')
 
 city = st.text_input("Enter city name:")
 
